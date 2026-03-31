e462b43b4b25b3bf9b39839d08d844a8265e0cfc
salvasocas <64211726+salvasocas@users.noreply.github.com>
2026-03-31 19:35:58 +0200

    updated app context, them context, and implemented them in the app

---
+++ src/context/ThemeContext.tsx
@@ -0,0 +1,38 @@
+import {createContext, useEffect, useState} from 'react'
+import { useContext } from 'react';
+
+interface ThemeContextType {
+    theme: string;
+    toggleTheme: () => void;
+}
+
+const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
+
+export function ThemeProvider({children}: {children: React.ReactNode}) {
+
+    const [theme, setTheme] = useState(()=>localStorage.getItem('theme') || (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'));
+
+    // Update theme when state changes
+    useEffect(() => {
+        const root = window.document.documentElement;
+        root.classList.remove('light', 'dark');
+        root.classList.add(theme);
+        localStorage.setItem('theme', theme);
+    }, [theme]);
+
+    const toggleTheme = () => {
+        setTheme(prev => prev === 'light' ? 'dark' : 'light');
+    };
+
+    return <ThemeContext.Provider value={{theme, toggleTheme}}>
+        {children}
+    </ThemeContext.Provider>
+}
+
+export function useTheme() {
+    const context = useContext(ThemeContext);
+    if (!context) {
+        throw new Error('useTheme must be used within a ThemeProvider');
+    }
+    return context;
+}
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,21 @@
+import { useState } from "react"
+
 const Login = () => {
+
+    const [state, setState] = useState('login')
   return (
-    <div>Login</div>
+    <div>
+        <main className="login-page-container">
+            <form className="login-form">
+                <h2 className="text-3xl font-medium text-gray-900 dark:text-white">
+                    {state === 'login' ? 'Sign In' : 'Sign Up'}
+                <p className="mt-2 text-sm text-gray-500/90">
+                    {state === 'login' ? "Please enter your email and password." : "Please enter your details to create an account."}
+                </p>
+                </h2>
+            </form>
+        </main>
+    </div>
   )
 }
 
